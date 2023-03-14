b367a492d4abc0d2c4a751c2212d26b3a3d7492e
Andrew <papsujevicandrew@gmail.com>
2023-03-14 18:37:51 +0200

    task 1, task 3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
+let intervalId = null;
+let intervalStatus = false;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+function changeBodyBgColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+refs.btnStart.addEventListener('click', onStartClick);
+
+function onStartClick() {
+  if (intervalStatus) {
+    return;
+  }
+  intervalId = setInterval(() => {
+    changeBodyBgColor();
+  }, 1000);
+  intervalStatus = true;
+}
+refs.btnStop.addEventListener('click', onStopClick);
+
+function onStopClick() {
+  clearInterval(intervalId);
+  intervalStatus = false;
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,63 @@
+const refs = {
+  delay: document.querySelector('input[name=delay]'),
+  step: document.querySelector('input[name=step]'),
+  amount: document.querySelector('input[name=amount]'),
+  btn: document.querySelector('button'),
+};
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+refs.btn.addEventListener('click', onBtnClick);
+
+function onBtnClick(e) {
+  e.preventDefault();
+  if (
+    refs.amount.value === '' ||
+    refs.delay.value === '' ||
+    refs.step.value === ''
+  ) {
+    // preventDefault();
+    return alert('Все поля должны быть заполнены');
   }
+  createPromise(1, refs.delay.value)
+    .then(({ position, delay }) =>
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`)
+    )
+    .catch(({ position, delay }) =>
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`)
+    );
+
+  makeMorePromises();
+  clearInputs();
+}
+
+function makeMorePromises() {
+  let time = Number(refs.delay.value);
+  for (let i = 2; i <= refs.amount.value; i += 1) {
+    createPromise(i, (time += Number(refs.step.value)))
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) =>
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`)
+      );
+  }
+}
+
+function clearInputs() {
+  refs.amount.value = '';
+  refs.delay.value = '';
+  refs.step.value = '';
 }
